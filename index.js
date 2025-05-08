8345275585a14ef7dfe44895a73d36d6a1ebd523
mobin afshari <mma137813781378@gmail.com>
2025-05-08 22:28:32 +0330

    feat: some utility class added

--- index.js
+++ index.js
@@ -1,55 +1,31 @@
-// import http from "http";
-// import { add, sub } from "./app/math.js";
-// import { parse } from "url";
-
-// const server = http.createServer((req, res) => {
-//   const { pathname, query } = parse(req.url, true);
-//   console.log(pathname, query);
-//   const a = Number(query.a);
-//   const b = Number(query.b);
-
-//   res.setHeader("Content-Type", "application/json");
-
-//   if (pathname === "/add") {
-//     res.end(JSON.stringify({ result: add(a, b) }));
-//   } else if (pathname === "/sub") {
-//     res.end(JSON.stringify({ result: sub(a, b) }));
-//   } else {
-//     res.statusCode = 404;
-//     res.end(JSON.stringify({ error: "Not Found" }));
-//   }
-// });
-
-// const PORT = 5000;
-
-// export function startServer() {
-//   return new Promise((resolve) => {
-//     server.listen(PORT, () => {
-//       console.log(`Server listening on http://localhost:${PORT}`);
-//       resolve();
-//     });
-//   });
-// }
-
-// export function stopServer() {
-//   return new Promise((resolve) => {
-//     server.close(() => resolve());
-//   });
-// }
-
-import fs from "fs/promises";
-import path from "path";
-
-async function runTestsInDir(dir) {
-  console.log(dir);
-  const files = await fs.readdir(dir);
-  const testFiles = files.filter((f) => f.endsWith(".test.js"));
-  console.log("===", testfiles);
-
-  for (const file of testFiles) {
-    await import(path.join(dir, file));
-  }
-
-  // await runAllTests();
+export function expect(value) {
+  return {
+    toBe(expected) {
+      if (value !== expected) {
+        throw new Error(`Expected ${value} to be ${expected}`);
+      }
+    },
+    toEqual(expected) {
+      if (JSON.stringify(value) !== JSON.stringify(expected)) {
+        throw new Error(
+          `Expected ${JSON.stringify(value)} to equal ${JSON.stringify(
+            expected
+          )}`
+        );
+      }
+    },
+    notToBe(expected) {
+      if (value === expected) {
+        throw new Error(`${value} is equal to ${expected}`);
+      }
+    },
+  };
 }
-runTestsInDir("test");
+export function Test(name, fn) {
+  try {
+    fn();
+    console.log(`${name} tests passed!`);
+  } catch (error) {
+    throw new Error(`${name} test failed!`);
+  }
+}
--- test/math.test.js
+++ test/math.test.js
@@ -1,46 +1,5 @@
-import { add, sub } from "../app/math.js";
-import { performance } from "node:perf_hooks";
+import { Test, expect } from "../index.js";
 
-const tests = [];
-
-function test(name, fn) {
-  tests.push({ name, fn });
-}
-
-test("add(1, 2) should return 3", () => {
-  const result = add(1, 2);
-  if (result !== 3) throw new Error(`Expected 3, got ${result}`);
+Test("sqrt 4 equal 2", () => {
+  expect(Math.sqrt(4)).toBe(2);
 });
-
-test("sub(5, 3) should return 2", () => {
-  const result = sub(5, 3);
-  if (result !== 2) throw new Error(`Expected 2, got ${result}`);
-});
-
-test("Math sqrt", () => {
-  const result = Math.sqrt(4);
-  if (result !== 2) throw new Error(`Expected 2, got ${result}`);
-});
-
-const start = performance.now();
-
-await Promise.allSettled(
-  tests.map(({ name, fn }) =>
-    Promise.resolve()
-      .then(() => {
-        const t0 = performance.now();
-        return fn(), performance.now() - t0;
-      })
-      .then((duration) => {
-        console.log(`✅ ${name} (${Math.round(duration)}ms)`);
-      })
-      .catch((err) => {
-        console.error(`❌ ${name}`);
-        console.error(err);
-        process.exit(1);
-      })
-  )
-);
-
-const end = performance.now();
-console.log(`\n🕒 Finished in ${Math.round(end - start)}ms`);
